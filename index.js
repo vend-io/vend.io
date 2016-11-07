06911f3f4db3cbaf34d6f2d71ce56dcca849292f
iwatakeshi <iwatakeshi@gmail.com>
2016-11-06 20:51:17 -0700

    Update
    Add exports.

--- index.js
+++ index.js
@@ -1,29 +1,9 @@
-const { VM } = require('./lib/core');
-const { Cash } = require('./lib/payment');
+const { Core } = require('./lib/core');
+const { Cash, Card } = require('./lib/payment');
 const Item = require('./lib/item').default;
 
-const vm = new VM();
-vm.inventory.addItem(new Item('1', 'Fanta Orange', 1.00, 2));
-vm.payment.method = new Cash();
-
-console.log(`Current state: ${vm.state.name}`);
-
-vm.selectById('1');
-
-console.log(`Current state: ${vm.state.name}`);
-
-vm.pay(0.50);
-console.log(`Paid (total): ${vm.payment.value}`);
-
-console.log(`Current state: ${vm.state.name}`);
-
-vm.selectById('1');
-
-console.log(`Current state: ${vm.state.name}`);
-
-vm.pay(0.50);
-console.log(`Paid (total): ${vm.payment.value}`);
-
-vm.selectById('1');
-
-console.log(`Current state: ${vm.state.name}`);
+module.exports = {
+  Core,
+  Payment: { Cash, Card },
+  Item
+};
